a4304c73af17a313b8a611ab41652d221c35a605
hilfee-founder <founder@hilfee.com>
2024-01-27 00:43:40 +0530

    add_deployed_url

--- src/Components/Utils/Constant.js
+++ src/Components/Utils/Constant.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    // url : "https://rheapi.azurewebsites.net" 
-    url: "http://localhost:8000" 
+    url : "https://courses-portal.onrender.com" 
+    // url: "http://localhost:8000" 
 
 });
